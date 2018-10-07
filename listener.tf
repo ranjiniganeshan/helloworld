8efac12d11ff4e656f4afc56ae8ce4e2bef86bc0
ranjiniganeshan <32661402+ranjiniganeshan@users.noreply.github.com>
2018-10-07 21:47:47 +0530

    Create listener.tf

---
+++ listener.tf
@@ -0,0 +1,10 @@
+resource "aws_alb_listener" "listener_http" {
+  load_balancer_arn = "${aws_alb.alb.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.group.arn}"
+    type             = "forward"
+  }
+}
