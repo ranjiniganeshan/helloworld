bec500ed533ea434e878cca5980b3b8cc2595c22
ranjiniganeshan <32661402+ranjiniganeshan@users.noreply.github.com>
2018-10-13 23:13:07 +0530

    Update listener.tf

--- listener.tf
+++ listener.tf
@@ -4,7 +4,8 @@ resource "aws_alb_listener" "listener_http" {
   protocol          = "HTTP"
 
   default_action {
-    target_group_arn = "${aws_alb_target_group.group.arn}"
-    type             = "forward"
+    target_group_arn = "${aws_alb_target_group.alb_target_group_two.arn}"
+    type = "forward"
   }
 }
+
