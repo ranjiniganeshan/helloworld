e30e2c031f7b891c5c72ce826e9c2a7d577a62b5
ranjiniganeshan <32661402+ranjiniganeshan@users.noreply.github.com>
2018-10-13 23:14:56 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,51 +1,21 @@
-variable "AWS_REGION" {
-  default = "us-east-1a"
-}
-
-variable "alb_name" {
-  default = "devfarm"
-}
-
+variable "AWS_REGION" {}
+variable "alb_name" {}
 variable "alb_subnets" {
-  default = ["subnet-011cac0c5005027b0", "subnet-0ea0539fb00cd6ccb"]
+type = "list"
+}
+variable "alb_security_groups"{
+type = "list"
 }
 
-variable "internal_alb" {
-  default = "false"
-}
-
-variable "s3_bucket" {
-  default = "devfarmbucket"
-}
-
-variable "idle_timeout" {
-  default = "60"
-}
-
-variable "alb_security_groups" {
-  default = ["sg-0ef0df721684a2d48", "sg-0baab316558d18aaf"]
-}
-
-variable "target_group_name" {
-  default = "devfarmtarget"
-}
-
-variable "svc_port" {
-  default = "443"
-}
-
-variable "vpc_id" {
-  default = "vpc-0bedfc6a799106996"
-}
-
-variable "target_group_sticky" {
-  default = "true"
-}
-
-variable "target_group_path" {
-  default = "/img"
-}
-
-variable "target_group_port" {
-  default = "443"
-}
+variable "internal_alb" {}
+variable "idle_timeout"{ }
+variable "s3_bucket" {}
+variable "svc_port" {}
+variable "aws_vpc" {}
+variable "target_group_sticky" {}
+variable "target_group_name_one" {}
+variable "target_group_path" {}
+variable "target_group_port" {}
+variable "target_group_name_two"{}
+variable "AMI" {}
+variable "PATH_TO_PUBLIC_KEY" {}
