58592dd7a5e185508d8a19ff9ad5947460e9ca03
ranjiniganeshan <32661402+ranjiniganeshan@users.noreply.github.com>
2018-10-13 23:13:40 +0530

    Update target_group.tf

--- target_group.tf
+++ target_group.tf
@@ -1,10 +1,34 @@
-resource "aws_alb_target_group" "alb_target_group" {  
-  name     = "${var.target_group_name}"  
+resource "aws_alb_target_group" "alb_target_group_one" {  
+  name     = "${var.target_group_name_one}"  
   port     = "${var.svc_port}"  
   protocol = "HTTP"  
-  vpc_id   = "${var.vpc_id}"   
+  vpc_id   = "${var.aws_vpc}"   
   tags {    
-    name = "${var.target_group_name}"    
+    name = "${var.target_group_name_one}"    
+  }   
+  stickiness {    
+    type            = "lb_cookie"    
+    cookie_duration = 1800    
+    enabled         = "${var.target_group_sticky}"  
+  }   
+  health_check {    
+    healthy_threshold   = 3    
+    unhealthy_threshold = 10    
+    timeout             = 5    
+    interval            = 10    
+    path                = "${var.target_group_path}"    
+    port                = "${var.target_group_port}"  
+  }
+}
+
+
+resource "aws_alb_target_group" "alb_target_group_two" {  
+  name     = "${var.target_group_name_two}"  
+  port     = "${var.svc_port}"  
+  protocol = "HTTP"  
+  vpc_id   = "${var.aws_vpc}"   
+  tags {    
+    name = "${var.target_group_name_two}"    
   }   
   stickiness {    
     type            = "lb_cookie"    
